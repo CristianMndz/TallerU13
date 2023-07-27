7225953dc210f2fc96fe00da37de69c4c5290c8d
CristianMndz <cristian11mendez@hotmail.com>
2023-07-26 22:28:19 -0500

    Arreglar saltos de línea

---
+++ src/conf_auth.js
@@ -0,0 +1,8 @@
+
+const conf_auth = {
+    domain: 'dev-o473vln6dzak50sh.us.auth0.com',
+    clientId: 'fZ8jXSwhqj9idNtBNNIyhVw1K2qHFuMA',
+    redirectUri: window.location.origin
+}
+
+export default conf_auth
